446fcc484aa0d06c04bd73d9babf930a6be411a2
Tynan DeBold <tynan.debold@tradeshift.com>
2019-03-03 20:33:26 +0100

    Updates

---
+++ precache-manifest.440ffe0547acdab272c8bf77ea4ff297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "210cc5f82c702b5893f8",
+    "url": "/url-parser/static/js/runtime~main.210cc5f8.js"
+  },
+  {
+    "revision": "7a9128366d33a425e1fe",
+    "url": "/url-parser/static/js/main.7a912836.chunk.js"
+  },
+  {
+    "revision": "f0e0bb2e58e8b5ccec82",
+    "url": "/url-parser/static/js/2.f0e0bb2e.chunk.js"
+  },
+  {
+    "revision": "7a9128366d33a425e1fe",
+    "url": "/url-parser/static/css/main.8ed0ec68.chunk.css"
+  },
+  {
+    "revision": "5ce2296a6eea65950f4a46581e13c9c2",
+    "url": "/url-parser/index.html"
+  }
+];
