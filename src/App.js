e5d4a6926620db6cd1257880bcf522338fbb072e
Tynan DeBold <tynan.debold@tradeshift.com>
2019-03-03 20:34:39 +0100

    refactored using hooks; added prettier

--- src/App.js
+++ src/App.js
@@ -1,67 +1,66 @@
 import './index.css';
-import React from 'react';
-
+import React, { useState } from 'react';
 import Form from './components/Form/';
 
-class App extends React.Component {
-  constructor() {
-    super();
+export default function App() {
+  const [message, setMessage] = useState('');
+  const [result, setResult] = useState('');
+  const [url, setUrl] = useState('');
 
-    this.state = {
-      message: '',
-      result: '',
-      url: '',
-    }
+  function handleChange(e) {
+    setUrl(e.target.value);
   }
 
-  handleChange = (e) => {
-    this.setState({url: e.target.value});
-  }
-
-  handleSubmit = (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    if (this.state.url.length === 0) {
-      return;
-    }
+    if (url.length === 0) return;
 
-    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(this.state.url);
+    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
     if (!valid) {
-      this.setState({ message: 'Please enter a valid URL.' });
+      setMessage('Please enter a valid URL.');
       return;
     }
 
-    this.parseUrl();
+    parseUrl();
   }
 
-  parseUrl() {
+  function parseUrl() {
     const parse = require('url-parse');
-    const url = parse(this.state.url, true);
-    this.setState({
-      message: '',
-      result: url
-    });
+    const parsedUrl = parse(url, true);
+    setMessage('');
+    setResult(parsedUrl);
   }
 
-  render() {
-    let output;
-    if (this.state.result) {
-      output = <pre>{JSON.stringify(this.state.result, undefined, 2)}</pre>;
-    }
-
-    return (
-      <React.Fragment>
-        <h1>URL Parser</h1>
-        <h3>Working on parsing a URL? Using something like <a href="https://www.npmjs.com/package/url-parse" target="_blank">this</a> or <a href="https://nodejs.org/docs/latest/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost" target="_blank">this</a>? Sometimes it's nice to see what's returned in your URL object.</h3>
-        <Form
-          submitForm={this.handleSubmit}
-          url={this.state.url}
-          updateInput={this.handleChange} />
-        {this.state.message}
-        {output}
-      </React.Fragment>
-    )
+  let output;
+  if (result) {
+    output = <pre>{JSON.stringify(result, undefined, 2)}</pre>;
   }
+  return (
+    <React.Fragment>
+      <h1>URL Parser</h1>
+      <h3>
+        Working on parsing a URL? Using something like{' '}
+        <a
+          href="https://www.npmjs.com/package/url-parse"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          this
+        </a>{' '}
+        or{' '}
+        <a
+          href="https://nodejs.org/docs/latest/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          this
+        </a>
+        ? Sometimes it's nice to see what's returned in your URL object.
+      </h3>
+      <Form submitForm={handleSubmit} url={url} updateInput={handleChange} />
+      {message}
+      {output}
+    </React.Fragment>
+  );
 }
-
-export default App;
